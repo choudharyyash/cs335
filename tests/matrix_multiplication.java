decb027b6685f052cfaf1b3f2080d49e7d00b1c3
choudharyyash <choudharyyash2012@gmail.com>
2019-04-16 19:39:18 +0530

    matrix mult case

--- tests/matrix_multiplication.java
+++ tests/matrix_multiplication.java
@@ -26,42 +26,55 @@ class matrix_multiplication{
         //     System.out.println(arr[i]);
         // }
 
-        int[][] firstarray = new int[2][2];
-        int[][] secondarray = new int[2][2];
-        firstarray[0][0] = 100;
+        int[][] firstarray = new int[3][3];
+        int[][] secondarray = new int[3][3];
+        int[][] result = new int[3][3];
+        firstarray[0][0] = 1;
         firstarray[0][1] = 2;
-        firstarray[1][0] = 3;
-        firstarray[1][1] = 4;
+        firstarray[0][2] = 3;
+        firstarray[1][0] = 4;
+        firstarray[1][1] = 5;
+        firstarray[1][2] = 6;
+        firstarray[2][0] = 7;
+        firstarray[2][1] = 8;
+        firstarray[2][2] = 9;
 
-        // secondarray[0][0] = 5;
-        // secondarray[0][1] = 6;
-        // secondarray[1][0] = 7;
-        // secondarray[1][1] = 8;
+        secondarray[0][0] = 10;
+        secondarray[0][1] = 11;
+        secondarray[0][2] = 12;
+        secondarray[1][0] = 13;
+        secondarray[1][1] = 14;
+        secondarray[1][2] = 15;
+        secondarray[2][0] = 16;
+        secondarray[2][1] = 17;
+
+        secondarray[2][2] = 18;
+
         /* Create another 2d array to store the result using the original arrays' lengths on row and column respectively. */
-        int[][] result = new int[2][2];
-        for (i = 0; i < 2; i++) {
-            for (k = 0; k < 2; k++) {
+
+        for (i = 0; i < 3; i++) {
+            for (k = 0; k < 3; k++) {
                 result[i][k] = 0;
-                System.out.println(firstarray[i][k]);
+                // System.out.println(firstarray[i][k]);
             }
         }
-        for (i = 0; i < 2; i++) {
-            for (k = 0; k < 2; k++) {
-                System.out.println(result[i][k]);
-            }
-        }
-        // /* Loop through each and get product, then sum up and store the value */
-        // for (i = 0; i < 2; i++) {
-        //     for (j = 0; j < 2; j++) {
-        //         for (k = 0; k < 2; k++) {
-        //             result[i][j] =  result[i][j] + firstarray[i][k] * secondarray[k][j];
-        //         }
+        // for (i = 0; i < 3; i++) {
+        //     for (k = 0; k < 3; k++) {
+        //         System.out.println(result[i][k]);
         //     }
         // }
+        /* Loop through each and get product, then sum up and store the value */
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                for (k = 0; k < 3; k++) {
+                    result[i][j] =  result[i][j] + firstarray[i][k] * secondarray[k][j];
+                }
+            }
+        }
 
         /* Show the result */
-        for (i = 0; i < 2; i++) {
-            for (k = 0; k < 2; k++) {
+        for (i = 0; i < 3; i++) {
+            for (k = 0; k < 3; k++) {
                 System.out.println(result[i][k]);
             }
         }
